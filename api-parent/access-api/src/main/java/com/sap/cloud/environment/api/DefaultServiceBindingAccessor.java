2b462466ba5a0a20c116e15ab64f60b561c5fdaa
Johannes Schneider <johannes.schneider03@sap.com>
2022-03-08 14:32:20 +0100

    remove locks in DefaultServiceBindingAccessor

--- api-parent/access-api/src/main/java/com/sap/cloud/environment/api/DefaultServiceBindingAccessor.java
+++ api-parent/access-api/src/main/java/com/sap/cloud/environment/api/DefaultServiceBindingAccessor.java
@@ -16,10 +16,6 @@ import java.util.stream.StreamSupport;
 public final class DefaultServiceBindingAccessor
 {
     @Nonnull
-    private static final ReadWriteLock instanceLock = new ReentrantReadWriteLock();
-    @Nonnull
-    private static final ReadWriteLock optionsLock = new ReentrantReadWriteLock();
-    @Nonnull
     private static ServiceBindingAccessor instance;
     @Nonnull
     private static ServiceBindingAccessorOptions options;
@@ -37,25 +33,15 @@ public final class DefaultServiceBindingAccessor
     @Nonnull
     public static ServiceBindingAccessor getInstance()
     {
-        instanceLock.readLock().lock();
-        try {
-            return instance;
-        } finally {
-            instanceLock.readLock().unlock();
-        }
+        return instance;
     }
 
     public static void setInstance( @Nullable final ServiceBindingAccessor accessor )
     {
-        instanceLock.writeLock().lock();
-        try {
-            if (accessor != null) {
-                instance = accessor;
-            } else {
-                instance = newDefaultInstance();
-            }
-        } finally {
-            instanceLock.writeLock().unlock();
+        if (accessor != null) {
+            instance = accessor;
+        } else {
+            instance = newDefaultInstance();
         }
     }
 
@@ -76,25 +62,15 @@ public final class DefaultServiceBindingAccessor
     @Nonnull
     public static ServiceBindingAccessorOptions getOptions()
     {
-        optionsLock.readLock().lock();
-        try {
-            return options;
-        } finally {
-            optionsLock.readLock().unlock();
-        }
+        return options;
     }
 
     public static void setOptions( @Nullable final ServiceBindingAccessorOptions options )
     {
-        optionsLock.writeLock().lock();
-        try {
-            if (options == null) {
-                DefaultServiceBindingAccessor.options = ServiceBindingAccessorOptions.NONE;
-            } else {
-                DefaultServiceBindingAccessor.options = options;
-            }
-        } finally {
-            optionsLock.writeLock().unlock();
+        if (options == null) {
+            DefaultServiceBindingAccessor.options = ServiceBindingAccessorOptions.NONE;
+        } else {
+            DefaultServiceBindingAccessor.options = options;
         }
     }
 }
