4c95f36e18264c2f579d3efb2dc9f35d62440c9b
Johannes Schneider <johannes-schneider_1995@web.de>
2022-05-03 13:14:06 +0200

    implement BindingMetadata and the BindingMetadataFactory

---
+++ service-binding-parent/sap-service-operator/src/main/java/com/sap/cloud/environment/servicebinding/metadata/PropertyFormat.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+
+package com.sap.cloud.environment.servicebinding.metadata;
+
+import javax.annotation.Nonnull;
+
+enum PropertyFormat
+{
+    TEXT("text"),
+    JSON("json");
+
+    @Nonnull
+    private final String value;
+
+    PropertyFormat( @Nonnull final String value )
+    {
+        this.value = value;
+    }
+
+    @Nonnull
+    public String getValue()
+    {
+        return value;
+    }
+}
--- service-binding-parent/sap-service-operator/src/test/java/com/sap/cloud/environment/servicebinding/TestResource.java
+++ service-binding-parent/sap-service-operator/src/test/java/com/sap/cloud/environment/servicebinding/TestResource.java
@@ -5,8 +5,11 @@
 package com.sap.cloud.environment.servicebinding;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -15,6 +18,19 @@ public final class TestResource
     private TestResource()
     {
         throw new IllegalStateException("This utility class must not be initialized.");
+    }
+
+    @Nonnull
+    public static String read( @Nonnull final Class<?> testClass, @Nonnull final String fileName )
+    {
+        final Path path = get(testClass, fileName);
+        try {
+            return String.join("\n", Files.readAllLines(path, StandardCharsets.UTF_8));
+        } catch (final IOException e) {
+            throw new AssertionError(String.format("Unable to read test resource from '%s/%s'",
+                                                   testClass.getSimpleName(),
+                                                   fileName), e);
+        }
     }
 
     @Nonnull
@@ -32,9 +48,9 @@ public final class TestResource
             rootFolder = Paths.get(url.toURI()).toString();
             return Paths.get(rootFolder, fileName);
         } catch (final URISyntaxException e) {
-            throw new AssertionError(String.format("Unable to load test source from '%s/%s'",
+            throw new AssertionError(String.format("Unable to load test resource from '%s/%s'",
                                                    testClass.getSimpleName(),
-                                                   fileName));
+                                                   fileName), e);
         }
     }
 }
