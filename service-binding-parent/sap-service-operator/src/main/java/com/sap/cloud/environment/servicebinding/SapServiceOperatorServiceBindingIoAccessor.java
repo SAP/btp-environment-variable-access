88598e18bc89b76820eb66ed9efd302c6402d3ca
Johannes Schneider <johannes-schneider_1995@web.de>
2022-05-18 08:42:18 +0200

    add tests for edge-cases and clean up implementation

--- service-binding-parent/sap-service-operator/src/main/java/com/sap/cloud/environment/servicebinding/SapServiceOperatorServiceBindingIoAccessor.java
+++ service-binding-parent/sap-service-operator/src/main/java/com/sap/cloud/environment/servicebinding/SapServiceOperatorServiceBindingIoAccessor.java
@@ -33,8 +33,7 @@ import com.sap.cloud.environment.servicebinding.metadata.BindingMetadata;
 import com.sap.cloud.environment.servicebinding.metadata.BindingMetadataFactory;
 import com.sap.cloud.environment.servicebinding.metadata.BindingProperty;
 
-public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindingAccessor
-{
+public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindingAccessor {
     @Nonnull
     public static final Function<String, String> DEFAULT_ENVIRONMENT_VARIABLE_READER = System::getenv;
 
@@ -61,22 +60,19 @@ public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindin
     @Nonnull
     private final Charset charset;
 
-    public SapServiceOperatorServiceBindingIoAccessor()
-    {
+    public SapServiceOperatorServiceBindingIoAccessor() {
         this(DEFAULT_ENVIRONMENT_VARIABLE_READER, DEFAULT_CHARSET);
     }
 
-    public SapServiceOperatorServiceBindingIoAccessor( @Nonnull final Function<String, String> environmentVariableReader,
-                                                       @Nonnull final Charset charset )
-    {
+    public SapServiceOperatorServiceBindingIoAccessor(@Nonnull final Function<String, String> environmentVariableReader,
+                                                      @Nonnull final Charset charset) {
         this.environmentVariableReader = environmentVariableReader;
         this.charset = charset;
     }
 
     @Nonnull
     @Override
-    public List<ServiceBinding> getServiceBindings() throws ServiceBindingAccessException
-    {
+    public List<ServiceBinding> getServiceBindings() throws ServiceBindingAccessException {
         final Path rootDirectory = getRootDirectory();
         if (rootDirectory == null) {
             return Collections.emptyList();
@@ -84,17 +80,16 @@ public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindin
 
         try (final Stream<Path> bindingRoots = Files.list(rootDirectory).filter(Files::isDirectory)) {
             return bindingRoots.map(this::parseServiceBinding)
-                               .filter(Optional::isPresent)
-                               .map(Optional::get)
-                               .collect(Collectors.toList());
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .collect(Collectors.toList());
         } catch (final IOException e) {
             return Collections.emptyList();
         }
     }
 
     @Nullable
-    private Path getRootDirectory()
-    {
+    private Path getRootDirectory() {
         final String maybeRootDirectory = environmentVariableReader.apply(ROOT_DIRECTORY_KEY);
         if (maybeRootDirectory == null || maybeRootDirectory.isEmpty()) {
             return null;
@@ -109,8 +104,7 @@ public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindin
     }
 
     @Nonnull
-    private Optional<ServiceBinding> parseServiceBinding( @Nonnull final Path rootDirectory )
-    {
+    private Optional<ServiceBinding> parseServiceBinding(@Nonnull final Path rootDirectory) {
         final Path metadataFile = rootDirectory.resolve(METADATA_FILE);
         if (!Files.exists(metadataFile) || !Files.isRegularFile(metadataFile)) {
             // every service binding must contain a metadata file
@@ -151,21 +145,20 @@ public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindin
         rawServiceBinding.put(credentialsKey, rawCredentials);
 
         final DefaultServiceBinding serviceBinding = DefaultServiceBinding.builder()
-                                                                          .copy(rawServiceBinding)
-                                                                          .withName(bindingName)
-                                                                          .withServiceName(maybeServiceName.get())
-                                                                          .withTagsKey(TAGS_KEY)
-                                                                          .withServicePlanKey(PLAN_KEY)
-                                                                          .withCredentialsKey(credentialsKey)
-                                                                          .build();
+                .copy(rawServiceBinding)
+                .withName(bindingName)
+                .withServiceName(maybeServiceName.get())
+                .withTagsKey(TAGS_KEY)
+                .withServicePlanKey(PLAN_KEY)
+                .withCredentialsKey(credentialsKey)
+                .build();
 
         return Optional.of(serviceBinding);
     }
 
-    private void addProperty( @Nonnull final Map<String, Object> properties,
-                              @Nonnull final Path rootDirectory,
-                              @Nonnull final BindingProperty property )
-    {
+    private void addProperty(@Nonnull final Map<String, Object> properties,
+                             @Nonnull final Path rootDirectory,
+                             @Nonnull final BindingProperty property) {
         final Optional<String> maybeValue = getPropertyFilePath(rootDirectory, property).flatMap(this::readFile);
         if (!maybeValue.isPresent()) {
             return;
@@ -175,45 +168,63 @@ public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindin
 
         switch (property.getFormat()) {
             case TEXT: {
-                properties.put(property.getName(), value);
+                addTextProperty(properties, property, value);
                 break;
             }
             case JSON: {
-                final String jsonWrapper = String.format("{\"content\": %s}", value);
-
-                final JSONObject jsonObject;
-                try {
-                    jsonObject = new JSONObject(jsonWrapper);
-                } catch (final JSONException e) {
-                    return;
-                }
-
-                if (!property.isContainer()) {
-                    // property is not a container, so the content should be attached as a flat value
-                    properties.put(property.getName(), jsonObject.get("content"));
-                    return;
-                }
-
-                // the property is a container, so we need to unpack it
-                final JSONObject content = jsonObject.getJSONObject("content");
-
-                for (final String key : content.keySet()) {
-                    properties.put(key, content.get(key));
-                }
-
+                addJsonProperty(properties, property, value);
                 break;
             }
             default: {
                 throw new IllegalStateException(String.format("The format '%s' is currently not supported",
-                                                              property.getFormat()));
+                        property.getFormat()));
             }
         }
     }
 
+    private void addTextProperty(@Nonnull final Map<String, Object> properties,
+                                 @Nonnull final BindingProperty property,
+                                 @Nonnull final String propertyValue) {
+        properties.put(property.getName(), propertyValue);
+    }
+
+    private void addJsonProperty(@Nonnull final Map<String, Object> properties,
+                                 @Nonnull final BindingProperty property,
+                                 @Nonnull final String propertyValue) {
+        // Wrap the property value inside another JSON object.
+        // This way we don't have to manually take care of correctly parsing the property type
+        // (it could be an integer, a boolean, a list, or even an entire JSON object).
+        // Instead, we can delegate the parsing logic to our JSON library.
+        final String jsonWrapper = String.format("{\"content\": %s}", propertyValue);
+
+        final JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(jsonWrapper);
+        } catch (final JSONException e) {
+            return;
+        }
+
+        if (!property.isContainer()) {
+            // property is not a container, so the content should be attached as a flat value
+            properties.put(property.getName(), jsonObject.get("content"));
+            return;
+        }
+
+        // the property is a container, so we need to unpack it
+        final JSONObject content = jsonObject.optJSONObject("content");
+        if (content == null) {
+            // the provided value is not a JSON object
+            return;
+        }
+
+        for (final String key : content.keySet()) {
+            properties.put(key, content.get(key));
+        }
+    }
+
     @Nonnull
-    private Optional<Path> getPropertyFilePath( @Nonnull final Path rootDirectory,
-                                                @Nonnull final BindingProperty property )
-    {
+    private Optional<Path> getPropertyFilePath(@Nonnull final Path rootDirectory,
+                                               @Nonnull final BindingProperty property) {
         final Path propertyFile = rootDirectory.resolve(property.getSourceName());
         if (!Files.exists(propertyFile) || !Files.isRegularFile(propertyFile)) {
             return Optional.empty();
@@ -223,8 +234,7 @@ public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindin
     }
 
     @Nonnull
-    private Optional<String> readFile( @Nonnull final Path path )
-    {
+    private Optional<String> readFile(@Nonnull final Path path) {
         try {
             return Optional.of(String.join("\n", Files.readAllLines(path, charset)));
         } catch (final IOException e) {
@@ -233,8 +243,7 @@ public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindin
     }
 
     @Nonnull
-    private Optional<String> getServiceName( @Nonnull final Map<String, Object> rawServiceBinding )
-    {
+    private Optional<String> getServiceName(@Nonnull final Map<String, Object> rawServiceBinding) {
         final Object maybeValue = rawServiceBinding.get(SERVICE_NAME_KEY);
         if (!(maybeValue instanceof String)) {
             return Optional.empty();
@@ -244,8 +253,7 @@ public class SapServiceOperatorServiceBindingIoAccessor implements ServiceBindin
     }
 
     @Nonnull
-    private String generateNewKey( @Nonnull final Map<String, Object> map )
-    {
+    private String generateNewKey(@Nonnull final Map<String, Object> map) {
         for (int i = 0; i < 100; ++i) {
             final String key = UUID.randomUUID().toString();
             if (map.containsKey(key)) {
