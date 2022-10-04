76a2d757afd9ee84ade81f78ef09b5f2befd8828
Johannes Schneider <johannes-schneider_1995@web.de>
2022-10-04 16:22:48 +0200

    Feat: Convenience for (partial) Customization (#66)
    add convenience method to do the Service Loader lookup

--- api-parent/access-api/src/main/java/com/sap/cloud/environment/servicebinding/api/ServiceBindingAccessor.java
+++ api-parent/access-api/src/main/java/com/sap/cloud/environment/servicebinding/api/ServiceBindingAccessor.java
@@ -5,6 +5,9 @@
 package com.sap.cloud.environment.servicebinding.api;
 
 import java.util.List;
+import java.util.ServiceLoader;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import javax.annotation.Nonnull;
 
@@ -16,6 +19,37 @@ import com.sap.cloud.environment.servicebinding.api.exception.ServiceBindingAcce
 @FunctionalInterface
 public interface ServiceBindingAccessor
 {
+    /**
+     * Returns {@link ServiceBindingAccessor} instances for implementations that are exposed via the
+     * <a href="https://xperti.io/blogs/understanding-java-service-loader/">Service Loader Pattern</a>.
+     * <p>
+     * </p>
+     * These instances are useful when the behavior of one (or more) specific {@link ServiceBindingAccessor}s should be
+     * overwritten while leaving others in their default state. <br>
+     * <b>Example:</b>
+     * 
+     * <pre>
+     * final List<ServiceBindingAccessor> defaultInstances = ServiceBindingAccessor.getInstancesViaServiceLoader();
+     * if( defaultInstances.removeIf(SapVcapServicesServiceBindingAccessor.class::isInstance) ) {
+     *     defaultInstances.add(new SapVcapServicesServiceBindingAccessor(customEnvironmentVariableReader));
+     * }
+     *
+     * final ServiceBindingMerger merger = new ServiceBindingMerger(defaultInstances, ServiceBindingMerger.KEEP_UNIQUE);
+     * final SimpleServiceBindingCache cache = new SimpleServiceBindingCache(merger);
+     *
+     * DefaultServiceBindingAccessor.setInstance(cache);
+     * </pre>
+     *
+     * @return A {@link List} of {@link ServiceBindingAccessor} instances created from implementations that are exposed
+     *         via the <i>Service Locator Pattern</i> (see above).
+     */
+    static List<ServiceBindingAccessor> getInstancesViaServiceLoader()
+    {
+        final ServiceLoader<ServiceBindingAccessor> serviceLoader =
+            ServiceLoader.load(ServiceBindingAccessor.class, ServiceBindingAccessor.class.getClassLoader());
+        return StreamSupport.stream(serviceLoader.spliterator(), false).collect(Collectors.toList());
+    }
+
     /**
      * Retrieves all {@link ServiceBinding}s that are accessible for this {@link ServiceBindingAccessor}.
      *
